bf72b327cb0c39bb6e0d379c04ea522df7f241ca
Makoto Yano <yan133@gmail.com>
2023-11-27 21:17:38 +0900

    対になるsimple-ts-serverと連動するようにした。
    ついでに、一般向けに公開されていないサブモジュールも排除。

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
-import { flexlog, PRIORITY } from 'common_libs/flexlog/flexlog';
-
 import serveApi from './serveApi';
 
 const projectName = process.env.npm_package_name !== undefined ?
   process.env.npm_package_name:
   'scops-own-server';
-
-const logger = flexlog.logger;
 
 logger.noizy();
 logger.setLogLevel(PRIORITY.info);
--- src/serveApi.ts
+++ src/serveApi.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export default function serveApi() {
   const app = express();
@@ -6,6 +7,12 @@ export default function serveApi() {
 
   app.get('/', (req, res) => {
     res.send('Hello! I\'m working!!');
+  });
+
+  app.use('/simple-ts/static/', express.static('../simple-ts-viewer/public/'));
+  app.get('/simple-ts/view/*', (req, res) => {
+    res.sendFile(
+      path.join(__dirname, '../../simple-ts-viewer/public/index.html'));
   });
 
   return app.listen(3001);
